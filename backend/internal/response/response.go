6664780f6321392acdff88c8ca8acd47b9f65fad
husni_robani <husnir2005@gmail.com>
2025-03-02 01:38:24 +0700

    make response template

---
+++ backend/internal/response/response.go
@@ -0,0 +1,25 @@
+package response
+
+import "github.com/gin-gonic/gin"
+
+type response struct {
+	Message string `json:"message,omitempty"`
+	Data any `json:"data,omitempty"`
+	Errors any `json:"errors,omitempty"`
+}
+
+func Success(c *gin.Context, httpCode int, message string, data any) {
+	payload := response{
+		Message: message,
+		Data: data,
+	}
+	c.JSON(httpCode, payload)
+}
+
+func Error(c *gin.Context, httpCode int, errors any) {
+	payload := response{
+		Errors: errors,
+	}
+
+	c.JSON(httpCode, payload)
+}
