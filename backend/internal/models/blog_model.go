2d0d33fcbaae3886b7ecddc1c942d743e065b576
husni_robani <husnir2005@gmail.com>
2025-03-03 20:53:51 +0700

    add form tag for form-data

--- backend/internal/models/blog_model.go
+++ backend/internal/models/blog_model.go
@@ -1,8 +1,11 @@
 package models
 
+import "mime/multipart"
+
 type Blog struct {
 	Id int `json:"id,omitempty" validate:"numeric"`
-	Title string `json:"title" validate:"required"`
-	URL string	`json:"url" validate:"required,url"`
-	Image string `json:"image" validate:"required,filepath"`
+	Title string `json:"title" form:"title" validate:"required"`
+	URL string	`json:"url" form:"url" validate:"required,url"`
+	Image string `json:"image,omitempty" form:"image,omitempty"`
+	ImageFile *multipart.FileHeader
 }
