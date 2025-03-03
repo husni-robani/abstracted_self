6bd99b4537478606bce73a6a9a6fced6732d1a11
husni_robani <husnir2005@gmail.com>
2025-03-03 20:55:57 +0700

    add 3 blog routes

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -4,11 +4,17 @@ import (
 	"database/sql"
 
 	"github.com/gin-gonic/gin"
+	"github.com/husni-robani/abstracted_self/backend/internal/handlers"
 )
 
 func SetupRouter(db *sql.DB) *gin.Engine {
 	router := gin.Default()
-	
+	// set "multipart forms" data like image
+	handler := handlers.SetupHandler(db)
+
+	router.GET("/blogs", handler.BlogHandler.GetBlogs)
+	router.GET("/blogs/:id", handler.BlogHandler.GetBlogByID)
+	router.POST("/blogs", handler.BlogHandler.CreateBlog)
 
 	return router
 }
