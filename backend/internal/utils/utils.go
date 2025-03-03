b6a661827743f3d7990942fa66b7128cfaa7acfb
husni_robani <husnir2005@gmail.com>
2025-03-03 20:53:04 +0700

    add function for file validation

--- backend/internal/utils/utils.go
+++ backend/internal/utils/utils.go
@@ -1,10 +1,12 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"mime/multipart"
 	"os"
+	"slices"
 
 	"github.com/husni-robani/abstracted_self/backend/internal/logger"
 )
@@ -33,5 +35,25 @@ func SaveFile(fileHeader *multipart.FileHeader, saveDir string) error {
 	}
 
 	logger.Info.Printf("file saved successfully: %s", outFile.Name())
+	return nil
+}
+
+func ValidateFile(fileHeader *multipart.FileHeader, validTypes []string, maxSize int64) error {
+	if fileHeader.Size > maxSize {
+		return errors.New("image larger than 300 KB")
+	}
+
+	if  !slices.Contains(validTypes, fileHeader.Header.Get("Content-Type")) {
+		var validTypesStr string
+		for i, v := range validTypes{
+			if i == len(validTypes) - 1{
+				validTypesStr += fmt.Sprintf("and '%s'", v)
+			}else {
+				validTypesStr += fmt.Sprintf("'%s', ", v)
+			}
+		}
+		return fmt.Errorf("content type is invalid, only receives %v", validTypesStr)
+	}
+
 	return nil
 }
