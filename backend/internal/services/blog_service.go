8642036eff55f11895b98b5b0027a79ec1a5675e
husni_robani <husnir2005@gmail.com>
2025-03-07 12:58:49 +0700

    create service method for delete and update blog :

--- backend/internal/services/blog_service.go
+++ backend/internal/services/blog_service.go
@@ -1,9 +1,12 @@
 package services
 
 import (
+	"os"
 	"path/filepath"
 
 	"github.com/google/uuid"
+	"github.com/husni-robani/abstracted_self/backend/internal/dto/requests"
+	"github.com/husni-robani/abstracted_self/backend/internal/logger"
 	"github.com/husni-robani/abstracted_self/backend/internal/models"
 	"github.com/husni-robani/abstracted_self/backend/internal/repositories"
 	"github.com/husni-robani/abstracted_self/backend/internal/utils"
@@ -56,4 +59,72 @@ func (service BlogService) CreateBlog(blog models.Blog) error {
 	}
 
 	return nil
+}
+
+func (service BlogService) DeleteBlog(id int) error {
+	blog, err := service.Repository.GetBlogByID(id)
+	if err != nil {
+		return err
+	}
+
+	if err := service.Repository.DeleteBlog(blog.Id); err != nil {
+		return err
+	}
+
+	if err := os.Remove("./storage/blog_images/" + blog.Image); err != nil {
+		logger.Error.Printf("error delete image: %v", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (service BlogService) UpdateBlog(id int, newBlogData requests.UpdateBlogRequest) (*models.Blog, error) {
+	blog, err := service.Repository.GetBlogByID(id)
+	if err != nil {
+		logger.Error.Printf("error while get blog data: %v", err)
+		return nil, err
+	}
+
+	var values = make(map[string]any)
+
+	if newBlogData.ImageFile != nil {
+		// save image
+		extension := filepath.Ext(newBlogData.ImageFile.Filename)
+		newFilename := uuid.New().String() + extension
+		
+		newBlogData.ImageFile.Filename = newFilename
+		if err := utils.SaveFile(newBlogData.ImageFile, "./storage/blog_images"); err != nil {
+			logger.Error.Printf("error save image to storage: %v", err.Error())
+			return nil, err
+		}
+		logger.Info.Printf("file saved: %v", newBlogData.ImageFile.Filename)
+		
+		// remove older image
+		err := os.Remove("./storage/blog_images/" + blog.Image)
+		if err != nil {
+			logger.Error.Printf("error remove image from storage: %v", err.Error())
+			return nil, err
+		}
+		values["image"] = newFilename
+	}
+
+	// save data to database
+	if newBlogData.Title != nil {
+		values["title"] = *newBlogData.Title
+	} 
+	if newBlogData.URL != nil {
+		values["url"] = *newBlogData.URL
+	} 
+
+	if err := service.Repository.UpdateBlog(id, values); err != nil {
+		return nil, err
+	}
+
+	updatedBlog, err := service.Repository.GetBlogByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &updatedBlog, nil
 }
