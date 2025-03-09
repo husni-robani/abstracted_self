32381de3e1e32ca654e0dd779fab82ad041c5a8a
husni_robani <husnir2005@gmail.com>
2025-03-09 16:00:51 +0700

    create authentication middleware

---
+++ backend/internal/auth/auth.go
@@ -0,0 +1,51 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/husni-robani/abstracted_self/backend/internal/logger"
+	"github.com/husni-robani/abstracted_self/backend/internal/utils"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := utils.GetToken(c.GetHeader("Authorization"))
+
+		if tokenString != os.Getenv("AUTH_TOKEN") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "Access denied",
+			})
+			return
+		}
+		
+		token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("invalid signing method: %v", t.Header["alg"])
+			}
+
+			return []byte(os.Getenv("AUTH_KEY")), nil
+		})
+
+		if err != nil {
+			logger.Info.Println(err.Error())
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "Access denied",
+			})
+			return
+		}
+
+		if _, ok := token.Claims.(*jwt.RegisteredClaims); !ok || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "Access denied",
+			})
+			return
+		}
+
+
+		c.Next()
+	}
+}
