1741822a43d9e11889c209bcea80d379c7357533
husni_robani <husnir2005@gmail.com>
2025-03-10 01:46:17 +0700

    create service for technology types

--- backend/internal/services/technologyType_service.go
+++ backend/internal/services/technologyType_service.go
@@ -1,6 +1,9 @@
 package services
 
-import "github.com/husni-robani/abstracted_self/backend/internal/repositories"
+import (
+	"github.com/husni-robani/abstracted_self/backend/internal/models"
+	"github.com/husni-robani/abstracted_self/backend/internal/repositories"
+)
 
 type TechnologyTypeService struct {
 	repository repositories.TechnologyTypeRepository
@@ -8,4 +11,31 @@ type TechnologyTypeService struct {
 
 func NewTechnologyTypeService(repository repositories.TechnologyTypeRepository) TechnologyTypeService {
 	return TechnologyTypeService{repository: repository}
+}
+
+func (service TechnologyTypeService) GetAllTypes() ([]models.TechnologyType, error) {
+	techTypes, err := service.repository.GetAllType()
+	if err != nil {
+		return nil, err
+	}
+
+	return techTypes, nil
+}
+
+func (service TechnologyTypeService) CreateTypes(typeNames []string) error {
+	err := service.repository.CreateTypes(typeNames)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (service TechnologyTypeService) DeleteTypeByID(id int) error {
+	err := service.repository.DeleteType(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
