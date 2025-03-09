cd93aaa7af196e481e24be9a00d14bcd5cf901a5
husni_robani <husnir2005@gmail.com>
2025-03-09 15:57:34 +0700

    make login and logout routes

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 
 	"github.com/gin-gonic/gin"
+	"github.com/husni-robani/abstracted_self/backend/internal/auth"
 	"github.com/husni-robani/abstracted_self/backend/internal/handlers"
 )
 
@@ -12,11 +13,19 @@ func SetupRouter(db *sql.DB) *gin.Engine {
 	// set "multipart forms" data like image
 	handler := handlers.SetupHandler(db)
 
+	router.POST("/auth/login", handler.AuthHandler.Login)
+
 	router.GET("/blogs", handler.BlogHandler.GetBlogs)
 	router.GET("/blogs/:id", handler.BlogHandler.GetBlogByID)
 	router.POST("/blogs", handler.BlogHandler.CreateBlog)
 	router.DELETE("/blogs/:id", handler.BlogHandler.DeleteBlog)
 	router.PATCH("/blogs/:id", handler.BlogHandler.UpdateBlog)
+
+	authorizedRouter := router.Group("")
+	authorizedRouter.Use(auth.AuthMiddleware())
+	{
+		authorizedRouter.POST("/auth/logout", handler.AuthHandler.Logout)
+	}
 
 	return router
 }
