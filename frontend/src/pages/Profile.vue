5f0ae6cd34f9e7eb2c8fbc6269abb3453247c540
husni_robani <husnir2005@gmail.com>
2025-07-04 04:41:40 +0700

    added component for Hero section

--- frontend/src/pages/Profile.vue
+++ frontend/src/pages/Profile.vue
@@ -1,7 +1,10 @@
 <template>
-  <ProfileLayout></ProfileLayout>
+  <ProfileLayout>
+    <Hero></Hero>
+  </ProfileLayout>
 </template>
 
 <script setup>
 import ProfileLayout from "../layouts/ProfileLayout.vue";
+import Hero from "./partials/Hero.vue";
 </script>
---
+++ frontend/src/pages/partials/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="lg:max-w-5xl mx-auto flex items-center h-screen">
+    <div class="container">
+      <p class="-mb-1 text-lg font-roboto text-gray-400">hi, my name is</p>
+      <h1 class="mb-2 text-5xl md:text-8xl font-roboto text-gray-700">
+        Husni Robani
+      </h1>
+      <span
+        id="typed"
+        class="text-2xl md:text-6xl font-semibold text-gray-300"
+      ></span>
+      <p
+        class="w-full md:w-2/3 lg:w-1/2 mt-7 font-normal text-gray-500 text-base md:text-lg"
+      >
+        Fresh graduate in computer science, passionate about mastering software
+        development and eager to apply my knowledge to real-world projects.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
