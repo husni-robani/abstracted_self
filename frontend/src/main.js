4db46e6e6f8a4db0b86112138681671b211487c7
husni_robani <husnir2005@gmail.com>
2025-07-04 04:42:28 +0700

    added typed cdn and the logi

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,3 +3,19 @@ import "./style.css";
 import Profile from "./pages/Profile.vue";
 
 createApp(Profile).mount("#app");
+
+var typed = new Typed("#typed", {
+  strings: [
+    "Developer-in-Training",
+    "Craft digital experiences",
+    "Junior Developer",
+    "Backend engineer",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 2000,
+  cursorChar: "_",
+  shuffle: false,
+  smartBackspace: false,
+  loop: true,
+});
