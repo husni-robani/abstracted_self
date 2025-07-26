d0a0aaaa4efa1b2f51cbddc03d11904e86db279e
husni_robani <husnir2005@gmail.com>
2025-07-26 18:03:43 +0700

    added fetch api for name and summary

--- frontend/src/pages/partials/Hero.vue
+++ frontend/src/pages/partials/Hero.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <p class="-mb-1 text-lg font-roboto text-gray-400">hi, my name is</p>
       <h1 class="mb-2 text-5xl md:text-8xl font-roboto text-gray-700">
-        Husni Robani
+        {{ profile.name }}
       </h1>
       <span
         id="typed"
@@ -12,11 +12,40 @@
       <p
         class="w-full md:w-2/3 lg:w-1/2 mt-7 font-normal text-gray-500 text-base md:text-lg"
       >
-        Fresh graduate in computer science, passionate about mastering software
-        development and eager to apply my knowledge to real-world projects.
+        {{ profile.summary }}
       </p>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+
+const api_endpoint =
+  import.meta.env.VITE_API_URL + import.meta.env.VITE_GET_PROFILEDATA_ENDPOINT;
+
+const taglines = [];
+
+const profile = ref({
+  name: "",
+  summary: "",
+});
+
+onMounted(async () => {
+  try {
+    const response = await fetch(api_endpoint + "?name=true&summary=true");
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch name and summary profile. Status: ${response.status}`
+      );
+    }
+
+    const jsonResponse = await response.json();
+    profile.value.name = jsonResponse.data.name;
+    profile.value.summary = jsonResponse.data.summary;
+  } catch (e) {
+    console.log(e);
+  }
+});
+</script>
