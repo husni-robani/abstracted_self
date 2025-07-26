27e270831cea394f43433c47278a9eeebf2da306
husni_robani <husnir2005@gmail.com>
2025-07-26 18:04:25 +0700

    changed taglines from static data to dynamic

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,18 +4,23 @@ import Profile from "./pages/Profile.vue";
 
 createApp(Profile).mount("#app");
 
-var typed = new Typed("#typed", {
-  strings: [
-    "Developer-in-Training",
-    "Craft digital experiences",
-    "Junior Developer",
-    "Backend engineer",
-  ],
-  typeSpeed: 50,
-  backSpeed: 50,
-  backDelay: 2000,
-  cursorChar: "_",
-  shuffle: false,
-  smartBackspace: false,
-  loop: true,
-});
+// Setu typed js
+const api_endpoint =
+  import.meta.env.VITE_API_URL + import.meta.env.VITE_GET_PROFILEDATA_ENDPOINT;
+
+fetch(api_endpoint + "?taglines=true")
+  .then((res) => res.json())
+  .then((jsonResponse) => {
+    const taglines = jsonResponse.data.taglines;
+
+    var typed = new Typed("#typed", {
+      strings: taglines,
+      typeSpeed: 50,
+      backSpeed: 50,
+      backDelay: 2000,
+      cursorChar: "_",
+      shuffle: false,
+      smartBackspace: false,
+      loop: true,
+    });
+  });
