4011fcd6799c0fccf057b5188f21d16bb5f45d23
husni_robani <husnir2005@gmail.com>
2025-07-28 19:06:33 +0700

    added routes for profile, projects, and dashboard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Profile from "../pages/Profile.vue";
 import Login from "../pages/Login.vue";
+import ProfileManagement from "../pages/ProfileManagement.vue";
 
 // routes
 const routes = [
   { path: "/", name: "Welcome", component: Profile },
   { path: "/unlock", name: "Login", component: Login },
+  {
+    path: "/admin/profile",
+    name: "Profile Admin",
+    component: ProfileManagement,
+    meta: { title: "Profile" },
+  },
+  {
+    path: "/admin/dashboard",
+    name: "Dashboard",
+    meta: { title: "Dashboard" },
+  },
+  {
+    path: "/admin/projects",
+    name: "Projects",
+    meta: { title: "Projects" },
+  },
 ];
 
 const router = createRouter({
