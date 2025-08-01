971ba7cebaa1b47d86c3765b9a751ba4cb011d34
husni_robani <husnir2005@gmail.com>
2025-08-01 21:19:18 +0700

    made custom cors middleware

--- backend/internal/auth/auth.go
+++ backend/internal/auth/auth.go
@@ -52,3 +52,19 @@ func AuthMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func DefaultCorsConfig() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8081")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
