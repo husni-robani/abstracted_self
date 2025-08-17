3dc93fa16dcee57b744590dbe0df5a93ac2fe568
husni_robani <husnir2005@gmail.com>
2025-08-17 20:20:16 +0700

    added new attribute SkillSet to profile data and removed the old Skill

--- backend/internal/models/profile_model.go
+++ backend/internal/models/profile_model.go
@@ -6,6 +6,17 @@ type Profile struct {
 	Bio string `json:"bio"`
 	Taglines []string `json:"taglines"`
 	ResumeFileName string `json:"resume_file_name"`
-	Skills []string `json:"skills"`
+	SkillSet []SkillType `json:"skill_set"`
+}
+
+type SkillType struct {
+	TypeName string `json:"type_name"`
+	SkillItems []Skill `json:"skill_items"`
+}
+
+type Skill struct {
+	Name string `json:"name"`
+	IconFilename string `json:"icon_filename"`
+	IsMostUsed bool `json:"is_most_used"`
 }
 
