b009bc666b2d5baa20a92d2f427ce67bf528d48a
husni_robani <husnir2005@gmail.com>
2025-08-23 11:26:20 +0700

    added new field blog_snippet to blog model

--- backend/internal/models/blog_model.go
+++ backend/internal/models/blog_model.go
@@ -1,11 +1,9 @@
 package models
 
-import "mime/multipart"
-
 type Blog struct {
 	Id int `json:"id" validate:"numeric"`
 	Title string `json:"title" form:"title" validate:"required"`
 	URL string	`json:"url" form:"url" validate:"required,url"`
 	Image string `json:"image" form:"image"`
-	ImageFile *multipart.FileHeader
+	BlogSnippet string `json:"blog_snippet" form:"blog_snippet"`
 }
