e1bb02dc96471ecbe198e56d0967f82dff8cd619
husni_robani <husnir2005@gmail.com>
2025-08-31 16:06:02 +0700

    made dockerfile for frontend and remove port definition in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: "8081",
-  },
   plugins: [vue(), tailwindcss()],
 });
